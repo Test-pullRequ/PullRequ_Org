8a762ad4a16bc771399c9d53ccf4105971f7c975
Test-pullRequ <58130138+Test-pullRequ@users.noreply.github.com>
2019-12-08 16:53:32 +0900

    修正01
    修正01

--- src/test01/Original.java
+++ src/test01/Original.java
@@ -4,7 +4,7 @@ public class Original {
 
 	public static void main(String[] args) {
 		System.out.println("このクラスはOriginalです。");
-		System.out.println("修正02を行いました。");
+		System.out.println("このクラスは修正01が行われました。");
 
 	}
 
